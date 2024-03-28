d60a895aedf16235549e997b53aa8d60f09b4aa6
TohirDev <isakulovtohir902@gmail.com>
2024-03-28 20:26:03 +0500

    modified: home page updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
+  const { t } = useTranslation();
   return (
     <div>
-      <ModeToggle />
-      <h1>home page</h1>
-      <Button variant={"default"}>go to login</Button>
+      <h1>{t("home-page")}</h1>
+      <Button variant={"default"}>
+        <NavLink to={"/login"}>{t("home-login")}</NavLink>
+      </Button>
+      <h1>{t("welcome")}</h1>
     </div>
   );
 }

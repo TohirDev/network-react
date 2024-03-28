738fd04e65538a1fa8528f1ea93ebcc4993e439d
TohirDev <isakulovtohir902@gmail.com>
2024-03-28 09:55:16 +0500

    feat: login page created

---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,90 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "username must be at least 2 chars",
+  }),
+});
+
+export const LoginPage = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">
+            {JSON.stringify(data, null, 2)}
+          </code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="grid place-items-center h-[100vh]">
+          <Card className="w-[350px] ">
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Do some stuffs</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <FormLabel>Username</FormLabel>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="username" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit">Login</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginPage;

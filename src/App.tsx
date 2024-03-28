37e90326ebccd6916ddda565848efb414c1cb69f
TohirDev <isakulovtohir902@gmail.com>
2024-03-28 20:27:28 +0500

    feat: container and header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "./components/theme-provider";
 import { Router } from "./routes";
+import i18n from "@/locals/i18n";
+import { Header } from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system">
-      <Router />
-    </ThemeProvider>
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider defaultTheme="system">
+        <Container>
+          <Header />
+          <Router />
+        </Container>
+      </ThemeProvider>
+    </I18nextProvider>
   );
 }
 

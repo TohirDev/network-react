bbb8e05154d5f4557926f80e9da0dbedfccaf402
TohirDev <isakulovtohir902@gmail.com>
2024-03-28 20:25:17 +0500

    modified: login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -20,34 +20,36 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { toast } from "@/components/ui/use-toast";
 import { Input } from "@/components/ui/input";
-
-const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "username must be at least 2 chars",
-  }),
-});
+import { FormInput } from "@/components/form/FromInput";
+import { useTranslation } from "react-i18next";
 
 export const LoginPage = () => {
+  const { t } = useTranslation();
+
+  const FormSchema = z.object({
+    username: z.string().min(4, {
+      message: t("login-valid-username"),
+    }),
+    password: z
+      .string()
+      .min(4, t("login-ps-num"))
+      .regex(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s)/,
+        t("login-ps-char")
+      ),
+  });
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       username: "",
+      password: "",
     },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">
-            {JSON.stringify(data, null, 2)}
-          </code>
-        </pre>
-      ),
-    });
+    console.log(data);
   }
 
   return (
@@ -57,7 +59,7 @@ export const LoginPage = () => {
           <Card className="w-[350px] ">
             <CardHeader>
               <CardTitle>Login</CardTitle>
-              <CardDescription>Do some stuffs</CardDescription>
+              <CardDescription>{t("login-title")}</CardDescription>
             </CardHeader>
             <CardContent>
               <FormLabel>Username</FormLabel>
@@ -70,11 +72,18 @@ export const LoginPage = () => {
                       <Input placeholder="username" {...field} />
                     </FormControl>
                     <FormDescription>
-                      This is your public display name.
+                      {t("login-username")}
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
+              />
+              <FormInput
+                control={form.control}
+                formDesc={t("login-password")}
+                formName="password"
+                placeHolder="Password"
+                type="password"
               />
             </CardContent>
             <CardFooter>

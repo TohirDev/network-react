fc4dac370bc5a3429105bf5aec088298d4b00edc
TohirDev <isakulovtohir902@gmail.com>
2024-03-31 22:42:16 +0500

    feat: token imported and loader added

--- src/pages/auth/login/funcs.ts
+++ src/pages/auth/login/funcs.ts
@@ -1,13 +1,26 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { TLoginResult } from "@/types";
+import { setTokenCookie } from "@/global";
+import { useNavigate } from "react-router-dom";
 
-export const useSubmitLogin = () => {
+export const useSubmitLogin = (): [
+  (
+    url: string,
+    data: {
+      username: string;
+      password: string;
+    }
+  ) => Promise<void>,
+  { loading: boolean }
+] => {
+  const navigate = useNavigate();
   const { toast } = useToast();
-
+  const [loading, setLoading] = useState(false);
   const onSubmit = useCallback(
     async (url: string, data: { username: string; password: string }) => {
       try {
+        setLoading(true);
         const response = await fetch(url, {
           method: "POST",
           headers: {
@@ -17,12 +30,15 @@ export const useSubmitLogin = () => {
         });
         const result: TLoginResult = await response.json();
         if (result.token) {
+          setTokenCookie(result.token, 30);
           toast({
             variant: "default",
             title: "Welcome",
             description: result.message,
           });
+          navigate("/profile");
         } else {
+          setLoading(false);
           toast({
             variant: "destructive",
             title: "Error",
@@ -31,9 +47,11 @@ export const useSubmitLogin = () => {
         }
       } catch (err) {
         throw new Error((err as Error).message);
+      } finally {
+        setLoading(false);
       }
     },
     [toast]
   );
-  return onSubmit;
+  return [onSubmit, { loading }];
 };

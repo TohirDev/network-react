5acd399a2c25f14f48fac680d79c066b6c891d67
TohirDev <isakulovtohir902@gmail.com>
2024-03-31 22:40:45 +0500

    modified: app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "./components/theme-provider";
 import { Router } from "./routes";
 import i18n from "@/locals/i18n";
-import { Header } from "./components/Header";
 import Container from "./components/Container";
+import { Layout } from "./layout";
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <ThemeProvider defaultTheme="system">
         <Container>
-          <Header />
-          <Router />
+          <Layout>
+            <Router />
+          </Layout>
         </Container>
       </ThemeProvider>
     </I18nextProvider>

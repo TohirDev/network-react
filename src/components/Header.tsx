a8b829bc7e6d936654c66534a2efcee7fb9e25e5
TohirDev <isakulovtohir902@gmail.com>
2024-03-31 22:41:41 +0500

    feaet: profile icon added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { ModeToggle } from "./mode-toggle";
+import { UserRound } from "lucide-react";
+import { Button } from "./ui/button";
 
 export const Header = () => {
   return (
@@ -23,6 +25,12 @@ export const Header = () => {
           alignItems: "center",
         }}
       >
+        <NavLink to={"/profile"}>
+          <Button variant="outline" size="icon" className="mr-5">
+            <UserRound />
+          </Button>
+        </NavLink>
+
         <ModeToggle />
         <LanguageSwitcher />
       </div>

0cd850e3816818b4c1d9ade124c36636235ba8b9
TohirDev <isakulovtohir902@gmail.com>
2024-04-04 21:33:50 +0500

    feat: registration page updated

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,21 +1,29 @@
-import { FormInput } from "@/components/form/FromInput";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardTitle,
+  CardHeader,
   CardContent,
   CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { Form, FormLabel } from "@/components/ui/form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FormInput } from "@/components/form/FromInput";
 import { useTranslation } from "react-i18next";
-import { z } from "zod";
+import { useCallback, useState } from "react";
 
-function RegistrationPage() {
+import { Toaster } from "@/components/ui/toaster";
+import { EyeIcon, EyeOffIcon, UserRound } from "lucide-react";
+import { TLoginResult } from "@/types";
+
+export const RegistrationPage = () => {
+  const [show, setShow] = useState(false);
   const { t } = useTranslation();
+
   const FormSchema = z.object({
     username: z.string().min(4, {
       message: t("login-valid-username"),
@@ -27,6 +35,10 @@ function RegistrationPage() {
         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s)/,
         t("login-ps-char")
       ),
+    firstname: z.string(),
+    lastname: z.string(),
+    email: z.string().email(),
+    picture: z.any(),
   });
 
   const form = useForm<z.infer<typeof FormSchema>>({
@@ -34,24 +46,110 @@ function RegistrationPage() {
     defaultValues: {
       username: "",
       password: "",
+      email: "",
+      firstname: "",
+      lastname: "",
+      picture: "",
     },
   });
+  const url = `${import.meta.env.VITE_BASE_API_URL}/signup`;
+  const onSubmit = useCallback(async (url: string, data: unknown) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result: TLoginResult = await response.json();
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit((data) => console.log(data))}>
+      <form onSubmit={form.handleSubmit((data) => onSubmit(url, data))}>
         <div className="grid place-items-center h-[80vh]">
-          <Card className="w-[350px]">
+          <Card className="w-[350px] ">
             <CardHeader>
               <CardTitle>Login</CardTitle>
               <CardDescription>{t("login-title")}</CardDescription>
             </CardHeader>
             <CardContent>
+              <FormLabel>Firstname</FormLabel>
+              <FormInput
+                formDesc={"This is your firstname"}
+                control={form.control}
+                formName="firstname"
+                placeHolder="Firstname"
+                suffix={
+                  <UserRound className="mt-3 cursor-pointer select-none" />
+                }
+              />
+              <FormLabel>Lastname</FormLabel>
+              <FormInput
+                formDesc={"Lastname"}
+                control={form.control}
+                formName="lastname"
+                placeHolder="Lastname"
+                suffix={
+                  <UserRound className="mt-3 cursor-pointer select-none" />
+                }
+              />
               <FormLabel>Username</FormLabel>
               <FormInput
+                formDesc={t("login-username")}
                 control={form.control}
-                formDesc="nimad"
                 formName="username"
-                placeHolder="deadass"
+                placeHolder="Username"
+                suffix={
+                  <UserRound className="mt-3 cursor-pointer select-none" />
+                }
+              />
+              <FormLabel>Password</FormLabel>
+              <FormInput
+                control={form.control}
+                formDesc={t("login-password")}
+                formName="password"
+                placeHolder="Password"
+                type={show ? "text" : "password"}
+                suffix={
+                  show ? (
+                    <EyeIcon
+                      className="mt-3 cursor-pointer select-none"
+                      onClick={() => setShow(false)}
+                    />
+                  ) : (
+                    <EyeOffIcon
+                      className="mt-3 cursor-pointer select-none"
+                      onClick={() => setShow(true)}
+                    />
+                  )
+                }
+              />
+              <FormLabel>Email</FormLabel>
+              <FormInput
+                formDesc={"This is your email"}
+                control={form.control}
+                formName="email"
+                placeHolder="email"
+                suffix={
+                  <UserRound className="mt-3 cursor-pointer select-none" />
+                }
+              />
+              <FormLabel>Picture</FormLabel>
+              <FormInput
+                formDesc={"Picture"}
+                control={form.control}
+                formName="picture"
+                placeHolder="Picture"
+                suffix={
+                  <UserRound className="mt-3 cursor-pointer select-none" />
+                }
+                type="file"
               />
             </CardContent>
             <CardFooter
@@ -61,14 +159,16 @@ function RegistrationPage() {
               }}
             >
               <Button className="w-[100%]" type="submit">
-                {"Sign up"}
+                Sign up
               </Button>
             </CardFooter>
           </Card>
         </div>
       </form>
+
+      <Toaster />
     </Form>
   );
-}
+};
 
 export default RegistrationPage;
